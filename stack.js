2754d64fe292321a6dac038fbe0e45e50ce1a889
tysondbv <140181582+tysondbv@users.noreply.github.com>
2024-06-10 08:19:09 +0530

    Update stack.js

--- stack.js
+++ stack.js
@@ -1,28 +1,28 @@
 function Stack() {
-this.stack = [];
+    this.stack = [];
 }
-Stack.prototype.push = function(element) {
-this.stack.push(element);
+Stack.prototype.push = function (element) {
+    this.stack.push(element);
 };
-Stack.prototype.pop = function() {
-let element = this.stack.pop();
-return element;
+Stack.prototype.pop = function () {
+    let element = this.stack.pop();
+    return element;
 };
-Stack.prototype.print = function() {
-if (!this.stack.length) {
-console.log("Empty");
-return;
-}
-let s = [].concat(this.stack);
-s.reverse();
-s.forEach(function println(item) {
-console.log(item);
-});
+Stack.prototype.print = function () {
+    if (!this.stack.length) {
+        console.log("Empty");
+        return;
+    }
+    let s = [].concat(this.stack);
+    s.reverse();
+    s.forEach(function println(item) {
+        console.log(item);
+    });
 };
-Stack.prototype.return_top = function() {
-return this.stack[this.stack.length - 1];
+Stack.prototype.return_top = function () {
+    return this.stack[this.stack.length - 1];
 };
-Stack.prototype.clear = function() {
-this.stack = [];
+Stack.prototype.clear = function () {
+    this.stack = [];
 };
 module.exports = Stack;
